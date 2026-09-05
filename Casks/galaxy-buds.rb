f17272ef83aab5d29b0f3ccfa57d7b645b8e6c47
Vedat Kılıç <kilicvedat@gmail.com>
2026-09-06 00:44:56 +0300

    galaxy-buds 1.1.0

--- Casks/galaxy-buds.rb
+++ Casks/galaxy-buds.rb
@@ -1,6 +1,6 @@
 cask "galaxy-buds" do
-  version "1.0.3"
-  sha256 "293754303dd3c8dfbe829afbf5006e350156b227460fb5e9aef942aa0628415f"
+  version "1.1.0"
+  sha256 "3c7af65a032e98f8a8a6273df9d28a8590ab9d8fc32ce41baf30b7b6a909a820"
 
   url "https://github.com/vedatkilic/galaxy-buds-mac/releases/download/v#{version}/Galaxy-Buds-#{version}.dmg"
   name "Galaxy Buds"
