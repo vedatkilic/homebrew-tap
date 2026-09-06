8858ba401ba74f26a990e741494087c905736c3c
Vedat Kılıç <kilicvedat@gmail.com>
2026-09-06 14:25:40 +0300

    Update galaxy-buds to 1.4.0

--- Casks/galaxy-buds.rb
+++ Casks/galaxy-buds.rb
@@ -1,6 +1,6 @@
 cask "galaxy-buds" do
-  version "1.3.0"
-  sha256 "a4451fa7adb4b91a90177d1908124e3ffc3ec59dcbaf192140cd3432553a8fc6"
+  version "1.4.0"
+  sha256 "87885aa52caedf0b9a1ead4676fd743d6cf0ead7f200b841b92bf4f30e5ffc1d"
 
   url "https://github.com/vedatkilic/galaxy-buds-mac/releases/download/v#{version}/Galaxy-Buds-#{version}.dmg"
   name "Galaxy Buds"
@@ -14,6 +14,7 @@ cask "galaxy-buds" do
     "~/Library/Preferences/com.nivorbit.budsapp.plist",
   ]
 end
+
 
 
 
